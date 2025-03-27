7873cb4edc230027ee890ef7384316d56f000f1c
finn79426 <finn79426@gmail.com>
2025-03-27 17:15:02 +0800

    update

---
+++ src/TransferEthers.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract Sender {
+
+    function transfer(address to) public {
+        payable(to).transfer(address(this).balance);
+    }
+
+    function send(address to) public {
+        payable(to).send(address(this).balance);
+    }
+
+    function call(address to) public {
+        payable(to).call{value: address(this).balance}("");
+    }
+
+    function callWithSuc(address to) public {
+        (bool suc, ) = payable(to).call{value: address(this).balance}("");
+    }
+
+    function callWithData(address to) public {
+        (bool suc, bytes memory data) = payable(to).call{value: address(this).balance}("");
+    }
+
+    function yulWithoutReturnData(address to) public {
+        bool suc;
+        assembly {
+            suc := call(
+                gas(),
+                to,
+                selfbalance(),
+                0,
+                0,
+                0,   
+                0    
+            )
+        }
+    }
+}
+
---
+++ test/TransferEthers.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Sender, Receiver} from "../src/TransferEthers.sol";
+
+contract SenderTest is Test  {
+    Sender sender;
+    function setUp() public {
+        sender = new Sender();
+        vm.deal(address(sender), 100 ether);
+    }
+
+    function test_transfer() public {
+        sender.transfer(address(makeAddr("foobar")));
+    }
+
+    function test_send() public {
+        sender.send(address(makeAddr("foobar")));
+    }
+
+    function test_call() public {
+        sender.call(address(makeAddr("foobar")));
+    }
+    
+    function test_callWithSuc() public {
+        sender.callWithSuc(address(makeAddr("foobar")));
+    }
+
+    function test_callWithData() public {
+        sender.callWithData(address(makeAddr("foobar")));
+    }
+
+    function test_yulWithoutReturnData() public {
+        sender.yulWithoutReturnData(address(makeAddr("foobar")));
+    }
+}
+
+
+// >>> forge test
+// [PASS] test_call() (gas: 45178)
+// [PASS] test_callWithData() (gas: 45125)
+// [PASS] test_callWithSuc() (gas: 45054)
+// [PASS] test_send() (gas: 44891)
+// [PASS] test_transfer() (gas: 44914)
+// [PASS] test_yulWithoutReturnData() (gas: 44902)
